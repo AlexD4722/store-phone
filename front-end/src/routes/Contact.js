830eac719c7528eba8394f257763ae88201a1801
hieutranHN <119092673+hieutranHN@users.noreply.github.com>
2023-08-08 21:07:26 +0700

    update contact
    update contact

--- front-end/src/routes/Contact.js
+++ front-end/src/routes/Contact.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import '../styles/contact.scss';
 
 function Contact() {
@@ -52,70 +54,81 @@ function Contact() {
     return (
         <>
             <div className="container mt-5">
-                <div className='questions-and-contact'>
-                    <span className='questions-and-contact__title'>You can ask us questions !</span>
-                    <p className='questions-and-contact__description'>Contact us for all your questions and opinions, or you can solve your problems in a shorter time with our contact offices.</p>
-                </div>
-                <div className='contactus mt-5 mb-5'>
-                    <div className="row">
-                        <div className="col-sm-4">
-                            <div className='contactus'>
-                                <p className='contactus__country'>United States</p>
-                                <h4 className='contactus__country-office'>United States Office</h4>
-                                <p className='contactus__address'>205 Middle Road, 2nd Floor, New York</p>
-                                <p className='contactus__phone'>+1 1234 567 88</p>
-                                <a className='contactus__link' href="#">info@example.com</a>
-                            </div>
+                <Row xs={1} sm={1} md={1} lg={1}>
+                    <Col>
+                        <div className='questions-and-contact'>
+                            <span className='questions-and-contact__title'>You can ask us questions !</span>
+                            <p className='questions-and-contact__description'>Contact us for all your questions and opinions, or you can solve your problems in a shorter time with our contact offices.</p>
                         </div>
-                        <div className="col-sm-4">
-                            <div className='contactus'>
-                                <p className='contactus__country'>United Kingdom</p>
-                                <h4 className='contactus__country-office'>United Kingdom Office</h4>
-                                <p className='contactus__address'>79 Manor Way, 2nd Floor, Great Fransham</p>
-                                <p className='contactus__phone'>+49 1234 567 88</p>
-                                <a className='contactus__link' href="#">contact@example.com</a>
-                            </div>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className='contactus'>
-                                <p className='contactus__country'>Germany</p>
-                                <h4 className='contactus__country-office'>Germany Office</h4>
-                                <p className='contactus__address'>Holstenwall 86, Sachsen-Anhalt, Zschornewitz</p>
-                                <p className='contactus__phone'>+44 1234 567 88</p>
-                                <a className='contactus__link' href="#">info@example.com</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
 
-                <div className="row">
-                    <div className="col-sm-6 p-3">
-                        <div className="contact-form p-5">
-                            <h3 className="contact_form_title">Get in Touch</h3>
-                            <p className="contact_form_dec">Quisque mattis tortor eu tristique sodales. Aenean sit amet justo nec sem vestibulum.</p>
+
+                <Row xs={1} sm={1} md={3} lg={3}>
+                    <Col>
+                        <div className='contactus'>
+                            <p className='contactus__country'>United States</p>
+                            <h4 className='contactus__country-office'>United States Office</h4>
+                            <p className='contactus__address'>205 Middle Road, 2nd Floor, New York</p>
+                            <p className='contactus__phone'>+1 1234 567 88</p>
+                            <a className='contactus__link' href="#">info@example.com</a>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className='contactus'>
+                            <p className='contactus__country'>United States</p>
+                            <h4 className='contactus__country-office'>United States Office</h4>
+                            <p className='contactus__address'>205 Middle Road, 2nd Floor, New York</p>
+                            <p className='contactus__phone'>+1 1234 567 88</p>
+                            <a className='contactus__link' href="#">info@example.com</a>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className='contactus'>
+                            <p className='contactus__country'>United States</p>
+                            <h4 className='contactus__country-office'>United States Office</h4>
+                            <p className='contactus__address'>205 Middle Road, 2nd Floor, New York</p>
+                            <p className='contactus__phone'>+1 1234 567 88</p>
+                            <a className='contactus__link' href="#">info@example.com</a>
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row xs={1} sm={1} md={2} lg={2}>
+                    <Col>
+                        <div className="contact-form">
+                            <h3 className="contact-form__title">Get in Touch</h3>
+                            <p className="contact-form__description">Quisque mattis tortor eu tristique sodales. Aenean sit amet justo nec sem vestibulum.</p>
                             <form onSubmit={handleSubmit}>
-                                <div className="form-group">
-                                    <label htmlFor="name">Name :</label>
-                                    {errors.name && <p className="error">{"* " + errors.name}</p>}
-                                    <input
-                                        type="text"
-                                        id="name"
-                                        value={name}
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
+                                <Row xs={1} sm={1} md={1} lg={2}>
+                                    <Col>
+                                        <div className="form-group">
+                                            <label htmlFor="name">Name :</label>
+                                            <div className='form-error'> {errors.name && <p className="error">{"* " + errors.name}</p>}</div>
+                                            <input
+                                                type="text"
+                                                id="name"
+                                                value={name}
+                                                onChange={(e) => setName(e.target.value)}
+                                            />
 
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email:</label>
-                                    {errors.email && <p className="error">{"* " + errors.email}</p>}
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
+                                        </div>
+                                    </Col>
+                                    <Col>
+                                        <div className="form-group">
+                                            <label htmlFor="email">Email:</label>
+                                            {errors.email && <p className="error">{"* " + errors.email}</p>}
+                                            <input
+                                                type="email"
+                                                id="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            />
 
-                                </div>
+                                        </div>
+                                    </Col>
+                                </Row>
+
                                 <div className="form-group">
                                     <label htmlFor="subject">Subject:</label>
                                     {errors.subject && <p className="error">{errors.subject}</p>}
@@ -140,20 +153,23 @@ function Contact() {
                                 <button type="submit">SEND</button>
                             </form>
                         </div>
-                    </div>
+                    </Col>
 
-                    <div className="col-sm-6 p-5">
-                        <div className="heading-title mb-5">
-                            <p className="heading-title__description">Nam maximus nunc a augue pulvinar, non euismod mauris tempus. Cras non elit vel magna molestie pellentesque in eu dui. Donec laoreet quis erat vitae finibus. Vestibulum enim eros, porta eget quam et, euismod dictum elit.</p>
+                    <Col>
+                        <div className="text-img">
+                            <div className="text-img__title">
+                                <p className="text-img__description">Nam maximus nunc a augue pulvinar, non euismod mauris tempus.
+                                    Cras non elit vel magna molestie pellentesque in eu dui. Donec laoreet quis erat vitae finibus.
+                                    Vestibulum enim eros, porta eget quam et, euismod dictum elit.</p>
+                            </div>
+
+                            <div className="img_contact">
+                                <img src="https://k4j3j2s7.rocketcdn.me/machic/wp-content/uploads/2021/10/contact-1.jpg" />
+                            </div>
                         </div>
-
-                        <div className='img_contact'>
-                            <img src="https://k4j3j2s7.rocketcdn.me/machic/wp-content/uploads/2021/10/contact-1.jpg" />
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+                    </Col>
+                </Row>
+            </div >
         </>
     );
 }
