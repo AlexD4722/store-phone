8168d5a8ea3ae0aa584d91b696e5ae7d1923c2e2
LamTran93 <lamtdth2208062@fpt.edu.vn>
2023-08-17 14:07:38 +0700

    thêm một chút cart

--- front-end/src/store/reducer.js
+++ front-end/src/store/reducer.js
@@ -1,5 +1,22 @@
-function reducer(action, payload){
-
+function reducer(state, action) {
+    switch (action.type) {
+        case "add":
+            let found = false;
+            let newState = [...state]
+            newState.forEach((productLine) => {
+                if (productLine.product.id === action.payload.product.id) {
+                    productLine.quantity += action.payload.quantity;
+                    found = true;
+                }
+            });
+            if (found) {
+                return newState;
+            } else {
+                return [...state, action.payload];
+            }
+        default:
+            return state;
+    }
 }
 
 export default reducer;
