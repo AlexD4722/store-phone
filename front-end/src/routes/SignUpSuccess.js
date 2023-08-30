21780ba38f74b5976d5d5ac0690b04557e6dfdd7
LamTran93 <lamtdth2208062@fpt.edu.vn>
2023-08-31 01:47:40 +0700

    minor update

--- front-end/src/routes/SignUpSuccess.js
+++ front-end/src/routes/SignUpSuccess.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 function SignUpSuccess() {
     const navigate = useNavigate();
@@ -19,11 +19,17 @@ function SignUpSuccess() {
             navigate("../../signin");
         }
     }, [timeleft]);
+    
     return (
-        <>
+        <div className="text-center mt-5">
             <h1>Successfully! </h1>
-            <div>Redirect to sign in page in {timeleft}</div>
-        </>
+            <div>
+                Redirect to sign in page in {timeleft}...or you can click{" "}
+                <Link to="../../signin" className="text-primary">
+                    here
+                </Link>
+            </div>
+        </div>
     );
 }
 
