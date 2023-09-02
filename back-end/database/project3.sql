bc96fe6a8d214b47db8cec234bc8a0b932c85390
AlexD4722 <119092789+AlexD4722@users.noreply.github.com>
2023-09-02 17:27:04 +0700

    update

--- back-end/database/project3.sql
+++ back-end/database/project3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th9 02, 2023 lúc 11:10 AM
+-- Thời gian đã tạo: Th9 02, 2023 lúc 12:26 PM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.0.28
 
@@ -89,14 +89,17 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`id`, `name`, `description`, `initial_price`, `selling_price`, `id_product_line`, `images`, `quantity`, `color`, `capacity`, `status`) VALUES
 (85, 'Iphone 14 Pro Max', '[\"Screen : 6.7 inch, Super Retina XDR, 2796 x 1290 Pixels\",\"Rear camera : 48.0 MP + 12.0 MP + 12.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Apple A16 Bionic Internal memory : 128GB\"]', 1500, 1350, 1, 'Iphone14ProMax', 50, 'blue', '125GB', 1),
 (86, 'Samsung Utra 23', '[\"Screen : 6.8 inch, Dynamic AMOLED 2X, QHD+, 1440 x 3088 Pixels\",\" Rear camera : 200.0 MP + 12.0 MP + 10.0 MP + 10.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Snapdragon 8 Gen 2\",\"Internal memory : 256GB\"]', 1000, 980, 2, 'SamsungUtra23', 30, 'black', '256gb', 1),
-(87, 'Iphone14 Pro Max', '[\"Screen : 6.7 inch, Super Retina XDR, 2796 x 1290 Pixels\",\"Rear camera : 48.0 MP + 12.0 MP + 12.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Apple A16 Bionic Internal memory : 128GB\"]', 1600, 1450, 1, 'Iphone14ProMax', 35, 'white', '256gb', 1),
+(87, 'Iphone 14 Pro Max', '[\"Screen : 6.7 inch, Super Retina XDR, 2796 x 1290 Pixels\",\"Rear camera : 48.0 MP + 12.0 MP + 12.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Apple A16 Bionic Internal memory : 128GB\"]', 1600, 1450, 1, 'Iphone14ProMax', 35, 'white', '256gb', 1),
 (88, 'Samsung Utra 23', '[\"Screen : 6.8 inch, Dynamic AMOLED 2X, QHD+, 1440 x 3088 Pixels\",\" Rear camera : 200.0 MP + 12.0 MP + 10.0 MP + 10.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Snapdragon 8 Gen 2\",\"Internal memory : 256GB\"]', 980, 850, 2, 'SamsungUtra23', 35, 'white', '128gb', 1),
-(89, 'Xiaomi Not 12 Pro', '[\"[\\\"Screen: 6.7 inch, extra: 1.9 inch, Dynamic AMOLED 2X, FHD+, 1080 x 2636 Pixels\\\",\\\"Rear camera :12.0 MP + 12.0 MP\\\",\\\"Camera Selfie : 10.0 MP\\\",\\\"CPU : Snapdragon 8+ Gen 1\\\",\\\"Internal memory : 128GB\\\"]\"]', 1110, 1035, 2, 'XiaomiNot12Pro', 55, 'black', '128gb', 1),
+(89, 'Xiaomi Not 12 Pro', '[\" Screen: 6.7 inch, extra: 1.9 inch, Dynamic AMOLED 2X, FHD+, 1080 x 2636 Pixels\",\"- Rear camera :12.0 MP + 12.0 MP\",\" Camera Selfie : 10.0 MP\",\" CPU : Snapdragon 8+ Gen 1\",\" Internal memory : 128GB\"]', 1110, 1035, 8, 'XiaomiNot12Pro', 55, 'black', '128gb', 1),
 (90, 'Samsung Utra 23', '[\"Screen : 6.8 inch, Dynamic AMOLED 2X, QHD+, 1440 x 3088 Pixels\",\" Rear camera : 200.0 MP + 12.0 MP + 10.0 MP + 10.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Snapdragon 8 Gen 2\",\"Internal memory : 256GB\"]', 1200, 1135, 2, 'SamsungUtra23', 25, 'black', '512gb', 1),
-(91, 'test', '[\"...\"]', 1000, 999, 2, 'EarphoneBeatsStudio3Wireless', 55, 'black', '512gb', 1),
-(92, 'test', '[\"adsds\"]', 100, 99, 2, 'EarphoneBeatsStudio3Wireless', 15, 'blue', '218gb', 1),
-(93, 'demo', '[\"...\"]', 999, 888, 1, 'EarphoneBeatsFlex', 15, 'back', '', 1),
-(94, 'demo', '[\"...\"]', 788, 555, 1, 'EarphoneBeatsFlex', 15, 'white', '', 1);
+(95, 'IPad Gen 10', '[\" Screen : 10.9 inch, Liquid Retina HD, 2360 x 1640 Pixels\",\" Rear camera : 12.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Apple A14 Bionic\",\"Internal memory : 64GB\"]', 1500, 1399, 7, 'iPadGen10', 50, 'black', '512gb', 1),
+(96, 'IPad Gen 10', '[\" Screen : 10.9 inch, Liquid Retina HD, 2360 x 1640 Pixels\",\" Rear camera : 12.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Apple A14 Bionic\",\"Internal memory : 64GB\"]', 1500, 1399, 7, 'iPadGen10', 50, 'black', '218gb', 1),
+(97, 'IPad Gen 10', '[\" Screen : 10.9 inch, Liquid Retina HD, 2360 x 1640 Pixels\",\" Rear camera : 12.0 MP\",\"Camera Selfie : 12.0 MP\",\"CPU : Apple A14 Bionic\",\"Internal memory : 64GB\"]', 1500, 1399, 7, 'iPadGen10', 50, 'black', '1TB', 1),
+(98, 'Earphone Beats Flex', '[\"[\\\"-Connectivity: The BeatsFitProTrueWirelessEarbuds headphones connect wirelessly to the device via Bluetooth technology.\\\",\\\"-Weight: Each Beats Flex headset weighs about 4 grams. Playtime: Beats Flex headphones provide up to 5 hours of music time and up to 3 hours of talk time on a full charge. Charging time: Beats Flex need about 15 minutes to charge enough for 3 hours of music or 2 hours of talk.\\\",\\\"-Charging box: The charging box that comes with the AirPods 2 helps to charge and store the headphones. The charging case can provide up to 24 hours of music time and up to 18 hours of talk time.\\\",\\\"-Sensors and controls: Beats Flex have motion sensors and noise sensors to adjust volume and play music automatically when placed in your ears.\\\"]\"]', 500, 499, 3, 'EarphoneBeatsFlex', 100, 'black', '', 1),
+(99, 'Earphone Beats Flex', '[\"[\\\"-Connectivity: The BeatsFitProTrueWirelessEarbuds headphones connect wirelessly to the device via Bluetooth technology.\\\",\\\"-Weight: Each Beats Flex headset weighs about 4 grams. Playtime: Beats Flex headphones provide up to 5 hours of music time and up to 3 hours of talk time on a full charge. Charging time: Beats Flex need about 15 minutes to charge enough for 3 hours of music or 2 hours of talk.\\\",\\\"-Charging box: The charging box that comes with the AirPods 2 helps to charge and store the headphones. The charging case can provide up to 24 hours of music time and up to 18 hours of talk time.\\\",\\\"-Sensors and controls: Beats Flex have motion sensors and noise sensors to adjust volume and play music automatically when placed in your ears.\\\"]\"]', 500, 499, 3, 'EarphoneBeatsFlex', 100, 'white', '', 1),
+(100, 'Lightning Charging Cable', '[\"[\\\"-Connectivity: The Lightning charging cable has a dedicated Lightning connector for connecting to Apple devices and a USB-A or USB-C plug for connecting to a power source or computer.\\\",\\\"-Data transfer speed: Lightning charging cable can support data transfer rate up to USB 2.0, allowing for fast data transfer between device and computer.\\\",\\\"-Length: Lightning charging cables are typically about 1-2 meters in length. However, there are also cable versions of different lengths to suit your needs.\\\",\\\"-Material: Lightning charging cables are usually made of high-quality materials such as PVC or nylon with break-resistant coating, which increases the strength and durability of the cable.\\\",\\\"-Functions: The Lightning charging cable is not only used to charge devices, but can also be used to connect other accessories such as speakers, headphones, or connect to an adapter for use with devices that do not have Lightning port.\\\"]\"]', 200, 180, 5, 'LightningChargingCable', 100, 'black', '', 1),
+(101, 'Lightning Charging Cable', '[\"[\\\"-Connectivity: The Lightning charging cable has a dedicated Lightning connector for connecting to Apple devices and a USB-A or USB-C plug for connecting to a power source or computer.\\\",\\\"-Data transfer speed: Lightning charging cable can support data transfer rate up to USB 2.0, allowing for fast data transfer between device and computer.\\\",\\\"-Length: Lightning charging cables are typically about 1-2 meters in length. However, there are also cable versions of different lengths to suit your needs.\\\",\\\"-Material: Lightning charging cables are usually made of high-quality materials such as PVC or nylon with break-resistant coating, which increases the strength and durability of the cable.\\\",\\\"-Functions: The Lightning charging cable is not only used to charge devices, but can also be used to connect other accessories such as speakers, headphones, or connect to an adapter for use with devices that do not have Lightning port.\\\"]\"]', 200, 185, 5, 'LightningChargingCable', 100, 'white', '', 1);
 
 -- --------------------------------------------------------
 
@@ -116,8 +119,13 @@ CREATE TABLE `product_line` (
 --
 
 INSERT INTO `product_line` (`id`, `brand`, `product_type`, `status`) VALUES
-(1, 'iphone', 'phone', 1),
-(2, 'samsung', 'phone', 1);
+(1, 'apple', 'phone', 1),
+(2, 'samsung', 'phone', 1),
+(3, 'beast', 'headphones', 1),
+(4, 'apple', 'headphones', 1),
+(5, 'samsung', 'accessories', 1),
+(7, 'apple', 'tablet', 1),
+(8, 'xiaomi', 'phone', 1);
 
 -- --------------------------------------------------------
 
@@ -231,13 +239,13 @@ ALTER TABLE `buyer_list`
 -- AUTO_INCREMENT cho bảng `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
 
 --
 -- AUTO_INCREMENT cho bảng `product_line`
 --
 ALTER TABLE `product_line`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT cho bảng `receipt_line`
